240c4e82c04f9b76ab3715555d32bc844edfe919
Gregory Thompson <gregt87@gmail.com>
2015-09-03 20:02:37 -0400

    cleaned up formatting a bit

--- src/clojure/com/example/gt/metime/main.clj
+++ src/clojure/com/example/gt/metime/main.clj
@@ -7,16 +7,13 @@
             [clojure.string :refer [join]]
             [neko.find-view :refer [find-view]]
             [neko.threading :refer [on-ui]])
-  (:import [android.widget CursorAdapter TextView]
+  (:import [android.widget CursorAdapter TextView LinearLayout]
            android.graphics.Color
            java.util.Calendar
-           android.app.Activity
+           [android.app Activity DialogFragment]
            android.view.View
-           android.widget.LinearLayout
-
            [android.app DatePickerDialog DatePickerDialog$OnDateSetListener]
-           [android.app TimePickerDialog TimePickerDialog$OnTimeSetListener]
-           android.app.DialogFragment))
+           [android.app TimePickerDialog TimePickerDialog$OnTimeSetListener]))
 
 (declare add-event)
 (declare date-picker)
@@ -29,22 +26,18 @@
 
 (def listing (atom (sorted-map)))
 
-
-
 (defn sorted-map-array-to-array-task [lst]
   (mapcat identity (map (fn [sorted-map-data]
-                          (into [] (map-indexed (fn [i ele]  (apply ->Task (concat [(first sorted-map-data)] ele [i]))) (second sorted-map-data)))
-                          ) lst)))
+                          (into [] (map-indexed (fn [i ele] (apply ->Task (concat [(first sorted-map-data)] ele [i]))) (second sorted-map-data))))
+                        lst)))
 
-
-(defn make-date-adapter [activity]
+(defn make-date-adapter []
   (adapters/ref-adapter
     (fn [_]
-      [:linear-layout {:id-holder true
+      [:linear-layout {:id-holder   true
                        :orientation :vertical}
        [:text-view {:id ::date-tv}]
-       [:text-view {:id ::event-tv}]
-       ])
+       [:text-view {:id ::event-tv}]])
     (fn [_ view _ task]
       (let [date-view (find-view view ::date-tv)
             event-view (find-view view ::event-tv)]
@@ -52,40 +45,32 @@
         (config date-view :visibility (if (= (:date-index task) 0) View/VISIBLE View/GONE))
         (config date-view :text (str (:date task)))
 
-        (config event-view :text (str (:name task) " " (:duration task) " " (:date-index task) ))
-
-        ))
+        (config event-view :text (str (:name task) " " (:duration task) " " (:date-index task)))))
     listing
     sorted-map-array-to-array-task))
 
 (defn main-layout [activity]
   [:linear-layout {:orientation :vertical}
-    [:edit-text {
-      :hint "Event name",
-      :id ::name}]
-    [:linear-layout {:orientation :horizontal}
-      [:text-view {
-        :hint "Goal (Time)",
-        :id ::time}]
-      [:button {
-        :text "...",
-        :on-click (fn [_] (show-picker activity
-          (time-picker activity), "timePicker"))}]]
-    [:linear-layout {:orientation :horizontal}
-      [:text-view {
-        :hint "Event date",
-        :id ::date}]
-      [:button {
-        :text "...",
-        :on-click (fn [_] (show-picker activity
-          (date-picker activity) "datePicker"))}]]
-    [:button {
-      :text "+ Event",
-      :on-click (fn [_] (add-event activity))}]
-    [:list-view {
-      :id ::days
-      :draw-selector-on-top true
-      :adapter (make-date-adapter activity)}]])
+   [:edit-text {
+                :hint "Event name",
+                :id   ::name}]
+   [:linear-layout {:orientation :horizontal}
+    [:text-view {:hint "Goal (Time)",
+                 :id   ::time}]
+    [:button {:text     "...",
+              :on-click (fn [_] (show-picker activity
+                                             (time-picker activity), "timePicker"))}]]
+   [:linear-layout {:orientation :horizontal}
+    [:text-view {:hint "Event date",
+                 :id   ::date}]
+    [:button {:text     "...",
+              :on-click (fn [_] (show-picker activity
+                                             (date-picker activity) "datePicker"))}]]
+   [:button {:text     "+ Event",
+             :on-click (fn [_] (add-event activity))}]
+   [:list-view {:id                   ::days
+                :draw-selector-on-top true
+                :adapter              (make-date-adapter)}]])
 
 (defn get-elmt [activity elmt]
   (str (.getText ^TextView (find-view activity elmt))))
@@ -100,13 +85,12 @@
   (swap! listing update-in [date-key] (fnil conj []) [event-name time-key]))
 
 (defn add-event [activity]
-  (let [
-      date-key (try
-        (read-string (get-elmt activity ::date))
-        (catch RuntimeException e "Date string is empty!"))
-      time-key (try
-        (read-string (get-elmt activity ::time))
-        (catch RuntimeException e "Time string is empty!"))]
+  (let [date-key (try
+                   (read-string (get-elmt activity ::date))
+                   (catch RuntimeException e "Date string is empty!"))
+        time-key (try
+                   (read-string (get-elmt activity ::time))
+                   (catch RuntimeException e "Time string is empty!"))]
     (when (number? date-key)
       (add-to-listing (get-elmt activity ::name) date-key time-key)
       (update-ui activity))))
@@ -115,13 +99,13 @@
   (proxy [DialogFragment DatePickerDialog$OnDateSetListener] []
     (onCreateDialog [savedInstanceState]
       (let [c (Calendar/getInstance)
-        year (.get c Calendar/YEAR)
-        month (.get c Calendar/MONTH)
-        day (.get c Calendar/DAY_OF_MONTH)]
+            year (.get c Calendar/YEAR)
+            month (.get c Calendar/MONTH)
+            day (.get c Calendar/DAY_OF_MONTH)]
         (DatePickerDialog. activity this year month day)))
     (onDateSet [view year month day]
       (set-elmt-text activity ::date
-        (format "%d%02d%02d" year (inc month) day)))))
+                     (format "%d%02d%02d" year (inc month) day)))))
 
 (defn time-picker [activity]
   (proxy [DialogFragment TimePickerDialog$OnTimeSetListener] []
@@ -129,11 +113,10 @@
       (TimePickerDialog. activity this 0 0 true))
     (onTimeSet [view hourOfDay minute]
       (set-elmt-text activity ::time
-        (format "%d"  (+ (* hourOfDay 60) minute))))))
+                     (format "%d" (+ (* hourOfDay 60) minute))))))
 
 (defactivity com.example.gt.metime.MainActivity
-  :key :main
-
-  (onCreate [this bundle]
-    (.superOnCreate this bundle)
-    (on-ui (set-content-view! (*a) (main-layout (*a))))))
+             :key :main
+             (onCreate [this bundle]
+                       (.superOnCreate this bundle)
+                       (on-ui (set-content-view! (*a) (main-layout (*a))))))
